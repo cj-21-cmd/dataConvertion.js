37837db86536f426e82ef4e1ff37547e813bf5aa
cjdelacruz <cjdelacruz0521@gmail.com>
2024-09-15 08:59:36 +0700

    Initial commit with dataConversion.js

---
+++ dataConversion.js
@@ -0,0 +1,20 @@
+//Convert a String to a Number
+let price = "200";
+price = parseInt(price);
+price = Number(price);
+price = + (price);
+console.log (price);
+console.log(typeof price);
+
+//Convert a Number to a String
+let distance = "150";
+distance = distance.toString();
+console.log (distance);
+console.log (typeof distance);
+
+//Convert a string into decimal
+let weight = "72.5"
+weight = parseFloat(weight);
+console.log(weight);
+console.log(typeof weight);
+
